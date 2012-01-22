97ea86635760664d207955c211c7228450a345ab
Sebastian Graf <sebastian.graf@uni-konstanz.de>
2012-01-22 12:29:52 +0100

    querying inserted node container, unncesseary fields removed

--- src/main/java/org/versacloud/HGDBCreateSample.java
+++ src/main/java/org/versacloud/HGDBCreateSample.java
@@ -3,8 +3,10 @@
  */
 package org.versacloud;
 
+import java.util.List;
+
 import org.hypergraphdb.HGHandle;
-import org.hypergraphdb.HGValueLink;
+import org.hypergraphdb.HGQuery.hg;
 import org.hypergraphdb.HyperGraph;
 import org.hypergraphdb.atom.HGBergeLink;
 import org.versacloud.model.Node;
@@ -17,33 +19,50 @@ public class HGDBCreateSample {
 
         byte[] material = { 0, 1, 2, 3, 4, 5 };
 
-        Node childNode = new Node("root1", 1l, 0, material);
+        Node childNode = new Node("leaf1", 1l, material);
+        Node rootNode = new Node("root1", 2l, material);
 
-        HGHandle handle1 = graph.add(childNode).getPersistent();
-        HGHandle handle2 = graph.add(new double[] { 0.9, 0.1, 4.3434 })
-                .getPersistent();
+        HGHandle[] handle1 = { graph.add(childNode) };
+        HGHandle[] handle2 = { graph.add(rootNode) };
 
-        handles(childNode, graph, handle1, handle2);
+        // handles(childNode, graph, handle1, handle2);
 
-        // HGBergeLink link = new HGBergeLink();
+        HGBergeLink link = new HGBergeLink(handle1, handle2);
+        graph.add(link);
+
+        query(graph);
 
     }
 
-    public static void handles(final Node node, final HyperGraph graph,
+    private static void query(final HyperGraph graph) {
+
+        // List nodes = hg.getAll(graph,
+        // hg.and(hg.type(Node.class), hg.eq("key", "1")));
+        List nodes = hg.getAll(graph, hg.type(Node.class));
+        for (Object n : nodes) {
+            System.out.println(n);
+        }
+    }
+
+    private static void handles(final Node node, final HyperGraph graph,
             final HGHandle handle1, final HGHandle handle2) {
         Object obj = graph.getHandle(node);
 
-        //Updating operation
-        node.setPrimaryKey(54);
+        // Updating operation
+        node.setKey(54);
         graph.update(node);
-        //removing of a handle and setting the node to annother handle
+        // removing of a handle and setting the node to annother handle
         graph.remove(handle1);
         graph.replace(handle2, node);
 
-        //see if reset works
+        // see if reset works
         obj = graph.getHandle(node);
 
-        //getting object
+        // Working with type handles -> Not working atm
+        // Object clazz1 = graph.getTypeSystem().getType(handle1);
+        // Object clazz2 = graph.getTypeSystem().getType(handle2);
+
+        // getting object
         obj = graph.get(handle2);
         System.out.println(obj);
     }
--- src/main/java/org/versacloud/model/Node.java
+++ src/main/java/org/versacloud/model/Node.java
@@ -3,34 +3,19 @@ package org.versacloud.model;
 public class Node {
 
     /**
-     * DAG key and primary key of database.
-     */
-    private long mDAGKey;
-
-    /**
      * Name of the node (group or user name).
      */
-    private String mName;
+    private String name;
 
     /**
      * Identifier of key.
      */
-    private long mKey;
-
-    /**
-     * Current version of node.
-     */
-    private int mVersion;
-
-    /**
-     * Last revision in key selector.
-     */
-    private long mLastRevSelectorKey;
+    private long key;
 
     /**
      * Secret key using for data en-/decryption.
      */
-    private byte[] mSecretKey;
+    private byte[] secretKey;
 
     /**
      * Standard constructor.
@@ -44,93 +29,53 @@ public class Node {
      * @param paramName
      *            node name.
      */
-    public Node(final String paramName, final long paramKey, final int paramVer, byte[] mSecretKey) {
-        this.mKey = paramKey;
-        this.mName = paramName;
-        this.mVersion = paramVer;
-        this.mSecretKey = mSecretKey;
-    }
-
-    /**
-     * Returns selector id.
-     * 
-     * @return
-     *         selector id.
-     */
-    public final long getPrimaryKey() {
-        return mDAGKey;
+    public Node(final String paramName, final long paramKey, byte[] mSecretKey) {
+        this.key = paramKey;
+        this.name = paramName;
+        this.secretKey = mSecretKey;
     }
 
     /**
      * Returns node name.
      * 
-     * @return
-     *         node name.
+     * @return node name.
      */
     public final String getName() {
-        return mName;
+        return name;
     }
 
     /**
-     * Sets new primary key. Usually not needed.
+     * Returns node name.
      * 
-     * @param paramKey
-     *            new key for node.
+     * @return node name.
      */
-    public final void setPrimaryKey(final long paramKey) {
-        this.mDAGKey = paramKey;
-    }
-
-    /**
-     * Returns current version of node.
-     * 
-     * @return
-     *         node's version.
-     */
-    public final int getVersion() {
-        return mVersion;
-    }
-
-    /**
-     * Increases node version by 1.
-     */
-    public final void increaseVersion() {
-        this.mVersion += 1;
+    public final void setName(String paramName) {
+        name = paramName;
     }
 
     /**
      * Returns secret key.
      * 
-     * @return
-     *         secret key.
+     * @return secret key.
      */
     public final byte[] getSecretKey() {
-        return mSecretKey;
+        return secretKey;
     }
 
     /**
      * Sets secret key.
      * 
-     * @return
-     *         secret key.
+     * @return secret key.
      */
     public final void setSecretKey(final byte[] mSecretKey) {
-        this.mSecretKey = mSecretKey;
-    }
-
-    public final void setRevSelKey(final long mSelKey) {
-        this.mLastRevSelectorKey = mSelKey;
-    }
-
-    public final long getLastRevSelKey() {
-        return mLastRevSelectorKey;
+        this.secretKey = mSecretKey;
     }
 
     public long getKey() {
-        return mKey;
+        return key;
     }
 
     public void setKey(long mKey) {
-        this.mKey = mKey;
+        this.key = mKey;
     }
 }
